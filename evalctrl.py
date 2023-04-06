ee44e5c20e8307eafb63325ba99a1d3820a3d338
Taoning Wang <taoning@gmail.com>
2023-04-06 11:09:33 -0700

    add scripts

---
+++ evalctrl.py
@@ -0,0 +1,188 @@
+import multiprocessing as mp
+from pathlib import Path
+import frads as fr
+from frads import methods, parsers
+import subprocess as sp
+import numpy as np
+import pandas as pd
+import sys
+
+
+sblinds = [
+    None,
+    'Objects/sblinds00.rad',
+    'Objects/sblinds10.rad',
+    'Objects/sblinds20.rad',
+    'Objects/sblinds30.rad',
+    'Objects/sblinds40.rad',
+    'Objects/sblinds50.rad',
+    'Objects/sblinds60.rad',
+    'Objects/sblinds70.rad',
+    'Objects/sblinds80.rad',
+]
+eblinds = [
+    None,
+    'Objects/eblinds00.rad',
+    'Objects/eblinds10.rad',
+    'Objects/eblinds20.rad',
+    'Objects/eblinds30.rad',
+    'Objects/eblinds40.rad',
+    'Objects/eblinds50.rad',
+    'Objects/eblinds60.rad',
+    'Objects/eblinds70.rad',
+    'Objects/eblinds80.rad',
+]
+wblinds = [
+    None,
+    'Objects/wblinds00.rad',
+    'Objects/wblinds10.rad',
+    'Objects/wblinds20.rad',
+    'Objects/wblinds30.rad',
+    'Objects/wblinds40.rad',
+    'Objects/wblinds50.rad',
+    'Objects/wblinds60.rad',
+    'Objects/wblinds70.rad',
+    'Objects/wblinds80.rad',
+]
+nblinds = [
+    None,
+    'Objects/nblinds00.rad',
+    'Objects/nblinds10.rad',
+    'Objects/nblinds20.rad',
+    'Objects/nblinds30.rad',
+    'Objects/nblinds40.rad',
+    'Objects/nblinds50.rad',
+    'Objects/nblinds60.rad',
+    'Objects/nblinds70.rad',
+    'Objects/nblinds80.rad',
+]
+skydir = Path('Sources') / 'perez'
+
+config = parsers.parse_mrad_config(Path("default.cfg"))
+
+views = [config['RaySender'][f'view{i}'].split() for i in range(1, 9)]
+
+with methods.assemble_model(config) as model:
+    mtx_paths = methods.three_phase(model, config)
+
+matrices = {'vmx': {}, 'dmx': {}}
+for i in range(1, 49):
+    matrices['vmx'][i] = fr.load_matrix(mtx_paths.pvmx[f'gridglazing{i}'])
+    matrices['dmx'][i] = fr.load_matrix(mtx_paths.dmx[f'glazing{i}'])
+    matrices['vmx']['door'] = fr.load_matrix(mtx_paths.pvmx['gridCCC-Doors-Glass'])
+    matrices['vmx']['skylight'] = fr.load_matrix(mtx_paths.pvmx['gridCCC-Skylights-Glass'])
+    matrices['dmx']['door'] = fr.load_matrix(mtx_paths.dmx['CCC-Doors-Glass'])
+    matrices['dmx']['skylight'] = fr.load_matrix(mtx_paths.dmx['CCC-Skylights-Glass'])
+
+bsdf = [fr.load_matrix(f"Resources/glazings/0blinds.xml")]
+for i in range(1, 10):
+    bsdf.append(fr.load_matrix(f"Resources/BSDF/blinds{(i-1)*10:02d}.xml"))
+
+room_ot = "scene.oct"
+
+epw_path = "Resources/Delft-2022.wea"
+with open(epw_path) as f:
+    meta, weas = fr.parse_wea(f.read())
+
+weas = [w for w in weas if w.time.hour >= 8 and w.time.hour <= 17]
+
+def evalctrl(wea_ctrl):
+    # ctrl = [0, 1, 0, 7]
+    # get illums
+    name = wea_ctrl[0]
+    wea = wea_ctrl[1]
+    dt_stamp = wea.time.strftime("%m%d_%H%M")
+    ctrl = wea_ctrl[2]
+    wpi = []
+    sky_mtx = fr.load_matrix(fr.genskymtx([wea], meta, mfactor=4, rotate=-22))
+    sky_path = f"Sources/perez/{dt_stamp}.rad"
+    window_wpis = np.zeros(294)
+    south_idx = range(1, 10)
+    east_idx = range(34, 49)
+    west_idx = range(10, 25)
+    north_idx = range(25, 34)
+    for si in south_idx:
+        window_wpis += fr.multiply_rgb(
+            matrices['vmx'][si], 
+            bsdf[int(ctrl[0])], 
+            matrices['dmx'][si], 
+            sky_mtx, 
+            weights=[47.4, 119.9, 11.6]).flatten()
+    for ei in east_idx:
+        window_wpis += fr.multiply_rgb(
+            matrices['vmx'][ei], 
+            bsdf[int(ctrl[1])], 
+            matrices['dmx'][ei], 
+            sky_mtx, 
+            weights=[47.4, 119.9, 11.6]).flatten()
+    for wi in west_idx:
+        window_wpis += fr.multiply_rgb(
+            matrices['vmx'][wi], 
+            bsdf[int(ctrl[2])], 
+            matrices['dmx'][wi], 
+            sky_mtx, 
+            weights=[47.4, 119.9, 11.6]).flatten()
+    for ni in north_idx:
+        window_wpis += fr.multiply_rgb(
+            matrices['vmx'][ni], 
+            bsdf[int(ctrl[3])], 
+            matrices['dmx'][ni], 
+            sky_mtx, weights=[47.4, 119.9, 11.6]).flatten()
+    window_wpis += fr.multiply_rgb(
+        matrices['vmx']['door'], 
+        bsdf[0], 
+        matrices['dmx']['door'], 
+        sky_mtx, 
+        weights=[47.4, 119.9, 11.6]).flatten()
+    window_wpis += fr.multiply_rgb(
+        matrices['vmx']['skylight'], 
+        bsdf[0], 
+        matrices['dmx']['skylight'], 
+        sky_mtx, 
+        weights=[47.4, 119.9, 11.6]).flatten()
+    wpi.append(window_wpis[:-8].mean())
+    # render
+    blinds = [sblinds[int(ctrl[0])], eblinds[int(ctrl[1])], wblinds[int(ctrl[2])], nblinds[int(ctrl[3])]]
+    blinds = [b for b in blinds if b is not None]
+    # Write a .rif file and run rad
+    rif = ["oconv= -i scene.oct"]
+    rif.append(f"scene= {str(sky_path)} {' '.join(blinds)}")
+    rif.append("ZONE=i 3.1 17 -24.48 -1.57 0 6.92")
+    rif.append("QUALITY=M")
+    rif.append("VARIABILITY=H")
+    rif.append("DETAIL=H")
+    rif.append("RESOLUTION=800")
+    rif.append(f"PICTURE={name}hdr/{name}_{dt_stamp}")
+    for vi, _ in enumerate(views):
+        rif.append(f"view=view{vi+1} -vf Views/view{vi+1}.vf")
+    # rif.append("INDIRECT=5")
+    # rif.append(f"AMBFILE={name}_{dt_stamp}"
+    # print("\n".join(rif))
+    rif_path = Path(f"{name}hdr/{name}_{dt_stamp}.rif")
+    with open(rif_path, "w") as f:
+        f.write("\n".join(rif))
+    sp.run(['rad', str(rif_path)])
+    # Run evalglare
+    dgps = []
+    for vi, _ in enumerate(views):
+        img = f"{name}hdr/{name}_{dt_stamp}_view{vi+1}.hdr"
+        proc = sp.check_output(['evalglare', '-i', str(window_wpis[-8+vi]), img])
+        dgps.append(proc.decode().split(':')[1].split()[0])
+    return wpi, window_wpis[-8:], dgps
+
+
+if __name__ == "__main__":
+    ctrl_path = Path(sys.argv[1])
+    name = ctrl_path.stem
+    ctrldf = pd.read_csv(ctrl_path, index_col=0, parse_dates=True)
+    wea_ctrls = []
+    for idx, wea in enumerate(weas):
+        ctrl = ctrldf.iloc[idx, :].tolist()
+        wea_ctrls.append([name, wea, ctrl])
+    pool = mp.Pool(mp.cpu_count())
+    result = pool.map(evalctrl, wea_ctrls)
+    df = pd.DataFrame((r[0] for r in result), columns=['avg_wpi'])
+    df[[f'ev{i}' for i in range(1, 9)]] = [r[1] for r in result]
+    df[[f'dgp{i}' for i in range(1, 9)]] = [r[2] for r in result]
+    df.set_index((wea.time for wea in weas), inplace=True)
+    df.to_csv(f"{name}_illum_dgp.csv")
---
+++ optimization.py
@@ -0,0 +1,166 @@
+from frads import methods, parsers
+from pathlib import Path
+import frads as fr
+import multiprocessing as mp
+import numpy as np
+import pandas as pd
+import pyradiance as pr
+import rbfopt
+import subprocess as sp
+
+
+# ## Read the configuration file
+config = parsers.parse_mrad_config(Path("default.cfg"))
+
+views = [config['RaySender'][f'view{i}'].split() for i in range(1, 9)]
+
+# ## Assemble the model and generate the matrices
+# Note: the "no_multiply" option is set to True in the configuration file
+with methods.assemble_model(config) as model:
+    mtx_paths = methods.three_phase(model, config)
+
+# ## Load view and daylight matrices into memory
+matrices = {'vmx': {}, 'dmx': {}}
+for i in range(1, 49):
+    matrices['vmx'][i] = fr.load_matrix(mtx_paths.pvmx[f'gridglazing{i}'])
+    matrices['dmx'][i] = fr.load_matrix(mtx_paths.dmx[f'glazing{i}'])
+matrices['vmx']['door'] = fr.load_matrix(mtx_paths.pvmx['gridCCC-Doors-Glass'])
+matrices['vmx']['skylight'] = fr.load_matrix(mtx_paths.pvmx['gridCCC-Skylights-Glass'])
+matrices['dmx']['door'] = fr.load_matrix(mtx_paths.dmx['CCC-Doors-Glass'])
+matrices['dmx']['skylight'] = fr.load_matrix(mtx_paths.dmx['CCC-Skylights-Glass'])
+
+# ## Load blinds BSDF into memory
+bsdf = [fr.load_matrix(f"Resources/glazings/0blinds.xml")]
+for i in range(1, 10):
+    bsdf.append(fr.load_matrix(f"Resources/BSDF/blinds{(i-1)*10:02d}.xml"))
+
+room_ot = "scene.oct"
+
+sblinds = [
+    None,
+    'Objects/sblinds00.rad',
+    'Objects/sblinds10.rad',
+    'Objects/sblinds20.rad',
+    'Objects/sblinds30.rad',
+    'Objects/sblinds40.rad',
+    'Objects/sblinds50.rad',
+    'Objects/sblinds60.rad',
+    'Objects/sblinds70.rad',
+    'Objects/sblinds80.rad',
+]
+eblinds = [
+    None,
+    'Objects/eblinds00.rad',
+    'Objects/eblinds10.rad',
+    'Objects/eblinds20.rad',
+    'Objects/eblinds30.rad',
+    'Objects/eblinds40.rad',
+    'Objects/eblinds50.rad',
+    'Objects/eblinds60.rad',
+    'Objects/eblinds70.rad',
+    'Objects/eblinds80.rad',
+]
+wblinds = [
+    None,
+    'Objects/wblinds00.rad',
+    'Objects/wblinds10.rad',
+    'Objects/wblinds20.rad',
+    'Objects/wblinds30.rad',
+    'Objects/wblinds40.rad',
+    'Objects/wblinds50.rad',
+    'Objects/wblinds60.rad',
+    'Objects/wblinds70.rad',
+    'Objects/wblinds80.rad',
+]
+nblinds = [
+    None,
+    'Objects/nblinds00.rad',
+    'Objects/nblinds10.rad',
+    'Objects/nblinds20.rad',
+    'Objects/nblinds30.rad',
+    'Objects/nblinds40.rad',
+    'Objects/nblinds50.rad',
+    'Objects/nblinds60.rad',
+    'Objects/nblinds70.rad',
+    'Objects/nblinds80.rad',
+]
+
+
+# ## Loop through the weather file and implement control
+epw_path = "Resources/Delft-2022.wea"
+with open(epw_path) as f:
+    meta, wea = parsers.parse_wea(f.read())
+
+
+def evalglare(img, ev):
+    proc = sp.run([Path(pr.__file__).parent/'bin'/'evalglare', '-i', ev], input=img, stderr=sp.PIPE, stdout=sp.PIPE)
+    return float(proc.stdout.decode().split(':')[1].split()[0])
+
+
+# Optimization using rbfopt (pyomo + bonmin for mixed integer non-linear programming)
+def main(w):
+    # print(w)
+    # Defining the objective function for optimization
+    def objfunc(x):
+        # print(x)
+        window_wpis = np.zeros(294)
+        south_idx = range(1, 10)
+        west_idx = range(10, 25)
+        north_idx = range(25, 34)
+        east_idx = range(34, 49)
+        for si in south_idx:
+            window_wpis += fr.multiply_rgb(matrices['vmx'][si], bsdf[int(x[0])], matrices['dmx'][si], _sky_mtx, weights=[47.4, 119.9, 11.6]).flatten()
+        for ei in east_idx:
+            window_wpis += fr.multiply_rgb(matrices['vmx'][ei], bsdf[int(x[1])], matrices['dmx'][ei], _sky_mtx, weights=[47.4, 119.9, 11.6]).flatten()
+        for wi in west_idx:
+            window_wpis += fr.multiply_rgb(matrices['vmx'][wi], bsdf[int(x[2])], matrices['dmx'][wi], _sky_mtx, weights=[47.4, 119.9, 11.6]).flatten()
+        for ni in north_idx:
+            window_wpis += fr.multiply_rgb(matrices['vmx'][ni], bsdf[int(x[3])], matrices['dmx'][ni], _sky_mtx, weights=[47.4, 119.9, 11.6]).flatten()
+        window_wpis += fr.multiply_rgb(matrices['vmx']['door'], bsdf[0], matrices['dmx']['door'], _sky_mtx, weights=[47.4, 119.9, 11.6]).flatten()
+        window_wpis += fr.multiply_rgb(matrices['vmx']['skylight'], bsdf[0], matrices['dmx']['skylight'], _sky_mtx, weights=[47.4, 119.9, 11.6]).flatten()
+        avg_wpi = window_wpis[:-8].mean()
+        evs = window_wpis[-8:]
+        blinds = [sblinds[int(x[0])], eblinds[int(x[1])], wblinds[int(x[2])], nblinds[int(x[3])]]
+        blinds = [b for b in blinds if b is not None]
+        tmpoct = f'temp{w.time.strftime("%m%d_%H%M")}.oct'
+        with open(tmpoct, 'wb') as f:
+            f.write(pr.oconv(_sky_path, *blinds, octree=room_ot))
+        dgps = []
+        # for v, ev in zip(views, evs):
+        for i, v  in enumerate(views):
+            img = pr.rpict(v, tmpoct, xres=800, yres=800, params=['-w', '-ab', '0', '-ps', '1'])
+            imgname = f"{'_'.join(map(str, x))}_view{i}.hdr"
+            with open(f"ctrltestimg/{imgname}.hdr", 'wb') as f:
+                f.write(img)
+            dgps.append(evalglare(img, str(evs[i])))
+        dgp = max(dgps)
+        if dgp < 0.38:
+            pen = 1
+        else:
+            pen = 0.0001
+        # with open("ctrltest.csv", "a") as wtr:
+        #     wtr.write(",".join(map(str, x)))
+        #     wtr.write(","+str(avg_wpi)+",")
+        #     wtr.write(",".join(map(str, evs)))
+        #     wtr.write(",")
+        #     wtr.write(",".join(map(str, dgps))+"\n")
+        return avg_wpi * pen * -1
+    _sky_mtx = fr.load_matrix(fr.genskymtx([w], meta, mfactor=4, rotate=-22))
+    _sky_path = f"Sources/perez/{w.time.strftime('%m%d_%H%M')}.rad"
+    bb = rbfopt.RbfoptUserBlackBox(4, np.array([0] * 4), np.array([9] * 4), np.array(['I'] * 4), objfunc)
+    settings = rbfopt.RbfoptSettings(max_evaluations = 10) 
+    alg = rbfopt.RbfoptAlgorithm(settings, bb)
+    objval, x, itercount, evalcount, fast_evalcount = alg.optimize()
+    return (w, x, objval)
+
+if __name__ == '__main__':
+    import time
+    wea = [w for w in wea if w.time.hour >= 8 and w.time.hour <= 17]
+    pool = mp.Pool(mp.cpu_count())
+    result = pool.map(main, wea)
+    df = pd.DataFrame([r[1] for r in result], columns=['south', 'east', 'west', 'north'])
+    df.set_index((r[0].time for r in result), inplace=True)
+    df.to_csv('optctrl.csv')
+    # stime = time.perf_counter()
+    # main(wea[0])
+    # print(time.perf_counter() - stime)
